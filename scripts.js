e3a8e568d97f56c869c68f0044bc896caeff3c68
saprolord <101603245+saprolord@users.noreply.github.com>
2022-03-21 18:55:22 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,57 +1,59 @@
 function factorycalc() {
-
-  'Recipes: Create the arrays and initalise the value'
-  'Array format: [[Factory ID, production rate],[material1,material2,...],[material1 requirement, material2 requirement, ...]]'
+  
+  'This is the main function to call out to create the recipe tree as selected in the "desired material" field at the rate defined in "desired rate"'
+  'This body sets up all the first level recipes as a MatDef class (see constructor further down)'
+  'MatDef class format: (name, factory:[Factory ID, production rate],recipe:[material1,material2,...],quantity: [material1 requirement, material2 requirement, ...])'
   'FactoryIDs: Extractor=0, Workshop=1, Furnace=2, Machine Shop=3, Industrial=4, Forge=5, Manufacturer=6, Earth Teleporter=7'
-  var wood_log = ["Wood Log", [0, 7.5], 0, [1]];
-  var copper = ["Copper", [0, 7.5], 0, [1]];
-  var iron = ["Iron", [0, 7.5], 0, [1]];
-  var stone = ["Stone", [0, 7.5], 0, [1]];
-  var wolfram = ["Wolframite", [0, 7.5], 0, [1]];
-  var coal = ["Coal", [0, 7.5], 0, [1]];
-  var wood_plank = ["Wood Plank", [1, 15], [wood_log], [1]];
-  var wood_frame = ["Wood Frame", [1, 7.5], [wood_plank], [4]];
-  var copper_ingot = ["Copper Ingot", [2, 30], [copper], [1]];
-  var copper_wire = ["Copper Wire", [1, 30], [copper_ingot], [1.5]];
-  var iron_ingot = ["Iron Ingot", [2, 30], [iron], [1]];
-  var iron_gear = ["Iron Gear", [1, 15], [iron_ingot], [2]];
-  var sand = ["Sand", [1, 40], [stone], [1]];
-  var silicon = ["Silicon", [2, 20], [sand], [2]];
-  var glass = ["Glass", [2, 10], [sand], [4]];
-  var tungsten = ["Tungsten Ore", [2, 24], [wolfram], [5]];
-  var graphite = ["Graphite", [5, 15], [coal, wood_log], [3, 3]];
-  var carbide = ["Tungsten Carbide", [5, 12], [tungsten, graphite], [2, 1]];
-  var coupler = ["Coupler", [1, 6], [carbide], [1]];
-  var lens = ["Condenser Lens", [1, 20], [glass], [3]];
-  var heat_sink = ["Heat Sink", [1, 10], [copper_ingot], [5]];
-  var iron_plate = ["Iron Plating", [1, 20], [iron_ingot], [2]];
-  var emagnet = ["Electromagnet", [3, 7.5], [iron_ingot, copper_wire], [6, 2]];
-  var metal_frame = ["Metal Frame", [3, 5], [wood_frame, iron_plate], [1, 4]];
-  var steel = ["Steel", [5, 7.5], [graphite, iron], [1, 6]];
-  var steel_rod = ["Steel Rod", [1, 15], [steel], [3]];
-  var rotor = ["Rotor", [3, 10], [steel_rod, iron_plate], [1, 2]];
-  var concrete = ["Concrete", [5, 7.5], [sand, steel_rod], [10, 1]];
-  var battery = ["Battery", [1, 2.5], [emagnet, graphite], [8, 8]];
-  var motor = ["Electric Motor", [5, 3], [battery, iron_gear, rotor], [1, 4, 2]];
-  var circuit = ["Logic Circuit", [3, 10], [copper_wire, silicon], [3, 2]];
-  var carbfibre = ["Carbon Fibre", [1, 7.5], [graphite], [4]];
-  var nanowire = ["Nano Wire", [3, 5], [carbfibre, glass], [2, 4]];
-  var computer = ["Computer", [4, 7.5], [heat_sink, metal_frame, circuit], [3, 1, 2]];
-  var ind_frame = ["Industrial Frame", [4, 3], [concrete, metal_frame, carbide], [6, 2, 8]];
-  var gyroscope = ["Gyroscope", [3, 5], [copper_wire, rotor], [12, 2]];
-  var stabilizer = ["Stabilizer", [4, 2.5], [computer, motor, gyroscope], [1, 1, 2]];
-  var mag_field = ["Magnetic Field Generator", [7, 1.5], [stabilizer, ind_frame, emagnet, nanowire], [1, 1, 10, 10]];
-  var quantum = ["Quantum Entangler", [3, 1], [mag_field, stabilizer], [1, 2]];
-  var microscope = ["Electron Microscope", [4, 2.5], [nanowire, emagnet, lens, metal_frame], [2, 8, 4, 2]];
-  var turbocharg = ["Turbocharger", [6, 4], [iron_gear, circuit, nanowire, coupler], [8, 4, 2, 4]];
-  var supercomp = ["Super Computer", [6, 2], [computer, heat_sink, turbocharg, coupler], [2, 8, 1, 8]];
-  var atomic = ["Atomic Locator", [6, 2], [supercomp, microscope, concrete, copper_wire], [2, 2, 24, 50]];
-  var energy_cube = ["Energy Cube", [3, 2], [battery, ind_frame], [2, 1]];
-  var tank = ["Tank", [4, 6], [glass, concrete, carbide], [2, 4, 4]];
-  var compressor = ["Matter Compressor", [6, 2], [ind_frame, turbocharg, motor, tank], [1, 2, 2, 1]];
-  var particle = ["Particle Glue", [1, 20], [compressor], [0.1]];
-  var duplicator = ["Matter Duplicator", [6, 2/3], [atomic, quantum, energy_cube, particle], [4, 2, 5, 100]];
-  var earth_token = ["Earth Token", [7, 60/42], [duplicator], [1]];
+
+  const wood_log = new MatDef("Wood Log", [0, 7.5], 0, [1]);
+  const copper = new MatDef("Copper", [0, 7.5], 0, [1]);
+  const iron = new MatDef("Iron", [0, 7.5], 0, [1]);
+  const stone = new MatDef("Stone", [0, 7.5], 0, [1]);
+  const wolfram = new MatDef("Wolframite", [0, 7.5], 0, [1]);
+  const coal = new MatDef("Coal", [0, 7.5], 0, [1]);
+  const wood_plank = new MatDef("Wood Plank", [1, 15], [wood_log], [1]);
+  const wood_frame = new MatDef("Wood Frame", [1, 7.5], [wood_plank], [4]);
+  const copper_ingot = new MatDef("Copper Ingot", [2, 30], [copper], [1]);
+  const copper_wire = new MatDef("Copper Wire", [1, 30], [copper_ingot], [1.5]);
+  const iron_ingot = new MatDef("Iron Ingot", [2, 30], [iron], [1]);
+  const iron_gear = new MatDef("Iron Gear", [1, 15], [iron_ingot], [2]);
+  const sand = new MatDef("Sand", [1, 40], [stone], [1]);
+  const silicon = new MatDef("Silicon", [2, 20], [sand], [2]);
+  const glass = new MatDef("Glass", [2, 10], [sand], [4]);
+  const tungsten = new MatDef("Tungsten Ore", [2, 24], [wolfram], [5]);
+  const graphite = new MatDef("Graphite", [5, 15], [coal, wood_log], [3, 3]);
+  const carbide = new MatDef("Tungsten Carbide", [5, 12], [tungsten, graphite], [2, 1]);
+  const coupler = new MatDef("Coupler", [1, 6], [carbide], [1]);
+  const lens = new MatDef("Condenser Lens", [1, 20], [glass], [3]);
+  const heat_sink = new MatDef("Heat Sink", [1, 10], [copper_ingot], [5]);
+  const iron_plate = new MatDef("Iron Plating", [1, 20], [iron_ingot], [2]);
+  const emagnet = new MatDef("Electromagnet", [3, 7.5], [iron_ingot, copper_wire], [6, 2]);
+  const metal_frame = new MatDef("Metal Frame", [3, 5], [wood_frame, iron_plate], [1, 4]);
+  const steel = new MatDef("Steel", [5, 7.5], [graphite, iron], [1, 6]);
+  const steel_rod = new MatDef("Steel Rod", [1, 15], [steel], [3]);
+  const rotor = new MatDef("Rotor", [3, 10], [steel_rod, iron_plate], [1, 2]);
+  const concrete = new MatDef("Concrete", [5, 7.5], [sand, steel_rod], [10, 1]);
+  const battery = new MatDef ("Battery", [1, 2.5], [emagnet, graphite], [8, 8]);
+  const motor = new MatDef("Electric Motor", [5, 3], [battery, iron_gear, rotor], [1, 4, 2]);
+  const circuit = new MatDef ("Logic Circuit", [3, 10], [copper_wire, silicon], [3, 2]);
+  const carbfibre = new MatDef ("Carbon Fibre", [1, 7.5], [graphite], [4]);
+  const nanowire = new MatDef("Nano Wire", [3, 5], [carbfibre, glass], [2, 4]);
+  const computer = new MatDef ("Computer", [4, 7.5], [heat_sink, metal_frame, circuit], [3, 1, 2]);
+  const ind_frame = new MatDef("Industrial Frame", [4, 3], [concrete, metal_frame, carbide], [6, 2, 8]);
+  const gyroscope = new MatDef("Gyroscope", [3, 5], [copper_wire, rotor], [12, 2]);
+  const stabilizer = new MatDef("Stabilizer", [4, 2.5], [computer, motor, gyroscope], [1, 1, 2]);
+  const mag_field = new MatDef("Mag. Field Generator", [7, 1.5], [stabilizer, ind_frame, emagnet, nanowire], [1, 1, 10, 10]);
+  const quantum = new MatDef("Quantum Entangler", [3, 1], [mag_field, stabilizer], [1, 2]);
+  const microscope = new MatDef("Electron Microscope", [4, 2.5], [nanowire, emagnet, lens, metal_frame], [2, 8, 4, 2]);
+  const turbocharg = new MatDef("Turbocharger", [6, 4], [iron_gear, circuit, nanowire, coupler], [8, 4, 2, 4]);
+  const supercomp = new MatDef("Super Computer", [6, 2], [computer, heat_sink, turbocharg, coupler], [2, 8, 1, 8]);
+  const atomic = new MatDef("Atomic Locator", [6, 2], [supercomp, microscope, concrete, copper_wire], [2, 2, 24, 50]);
+  const energy_cube = new MatDef("Energy Cube", [3, 2], [battery, ind_frame], [2, 1]);
+  const tank = new MatDef("Tank", [4, 6], [glass, concrete, carbide], [2, 4, 4]);
+  const compressor = new MatDef("Matter Compressor", [6, 2], [ind_frame, turbocharg, motor, tank], [1, 2, 2, 1]);
+  const particle = new MatDef("Particle Glue", [1, 20], [compressor], [0.1]);
+  const duplicator = new MatDef("Matter Duplicator", [6, 2/3], [atomic, quantum, energy_cube, particle], [4, 2, 5, 100]);
+  const earth_token = new MatDef("Earth Token", [7, 60/42], [duplicator], [1]);
 
 
   '________________________'
@@ -87,7 +89,7 @@ function factorycalc() {
 
   'Assign new rates in relation to the "tier" or "Level" assigned to each factories'
   for (i = 0; i < materialdata.length; i++) {
-    materialdata[i][1][1] = materialdata[i][1][1] * boostFactory[levelFactory[materialdata[i][1][0]]];
+    materialdata[i].factory[1] = materialdata[i].factory[1] * boostFactory[levelFactory[materialdata[i].factory[0]]];
   }
 
   const svg = document.querySelector('svg');
@@ -122,24 +124,25 @@ function calculate(material, rate) {
 	var start = ypos;
 
   'If the material is a Tier 1, write the input materials required in a column, then go back to the previous column'
-  if (material[2]==0) {
+
+  if (material.recipe==0) {
     if (xpos > svg.getAttribute('width')) {
       svg.setAttribute('width', xpos + 250);
     }
-    drawMatBox([material[0], rate, factoryname(material[1][0]), Math.ceil(rate / material[1][1])], [xpos, ypos]);
+    drawMatBox([material.name, rate, factoryname(material.factory[0]), Math.ceil(rate / material.factory[1])], [xpos, ypos]);
   } else {
-    drawMatBox([material[0], rate, factoryname(material[1][0]), Math.ceil(rate / material[1][1])], [xpos, ypos]);
-    if (material[2].length == 1) {
+    drawMatBox([material.name, rate, factoryname(material.factory[0]), Math.ceil(rate / material.factory[1])], [xpos, ypos]);
+    if (material.recipe.length == 1) {
       lineconnect([xpos + 150, ypos + 35], [xpos + 200, ypos + 35]);
       xpos = xpos + 200;
       if(xpos+200 > svg.getAttribute('width')) {
         svg.setAttribute('width', xpos + 250);
       }
-      calculate(material[2][0], material[3][0] * rate);
+      calculate(material.recipe[0], material.quantity[0] * rate);
       xpos = xpos - 200;
     } else {
 
-      for (var i = 0; i < material[2].length; i++) {
+      for (var i = 0; i < material.recipe.length; i++) {
           if (i > 0) {
           lineconnect([xpos + 192, start + 35], [xpos + 192, ypos + 135]);
           lineconnect([xpos + 192, ypos + 135], [xpos + 200, ypos + 135]);
@@ -154,7 +157,7 @@ function calculate(material, rate) {
         if (xpos+200 > svg.getAttribute('width')) {
           svg.setAttribute('width',xpos+210);
         }
-        calculate(material[2][i], material[3][i] * rate);
+        calculate(material.recipe[i], material.quantity[i] * rate);
         xpos = xpos - 200;
       }
     }
@@ -283,4 +286,17 @@ function drawMatBox(matdata, coord) {
 
 }
 
+class MatDef {
+'MatDef class is the definition of the material recipes as:'
+'name: name of the material as string'
+'factory: [factory name required, maximum rate of production]'
+'recipe: [list of material required]'
+'quantity: [list of quantity required for each material in the order defined in [recipe]]'
+  constructor(nameStr,factoryArr,recipeArr,quantityArr) {
+    this.name=nameStr;
+    this.factory=factoryArr;
+    this.recipe=recipeArr;
+    this.quantity=quantityArr;
+  }
+}
 
